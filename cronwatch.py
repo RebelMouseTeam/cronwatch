b8fe3b8ace275fcfdaf010637bb77dd1909d8542
Andrey Gubarev <mylokin@me.com>
2016-02-09 16:32:11 +0300

    working version of cronwatch

--- cronwatch.py
+++ cronwatch.py
@@ -1,11 +1,33 @@
 '''
+Cron's runner with integrated Sentry monitor
 '''
 
 __version__ = '0.1.0'
 
+import sys
+import contextlib
+
+
+@contextlib.contextmanager
+def args():
+    sys_argv = sys.argv[:]
+    sys.argv = sys.argv[1:]
+    yield
+    sys.argv = sys_argv
+
+
+def execute(fname):
+    try:
+        execfile(fname)
+    except:
+        print 'catch'
+        raise
+
 
 def main():
-    pass
+    fname = sys.argv[1]
+    with args():
+        execute(fname)
 
 if __name__ == '__main__':
     main()
