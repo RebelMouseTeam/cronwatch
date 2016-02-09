1a49930b6bf23634c6997f83cbb1ed5f8ce27ae8
Andrey Gubarev <mylokin@me.com>
2016-02-09 16:58:20 +0300

    update cronwatch

--- cronwatch.py
+++ cronwatch.py
@@ -4,30 +4,32 @@ Cron's runner with integrated Sentry monitor
 
 __version__ = '0.1.0'
 
+import os
 import sys
 import contextlib
+
+if os.getenv('DJANGO_SETTINGS_MODULE'):
+    from raven.contrib.django.raven_compat.models import client
+else:
+    import raven
+    client = raven.Client(dsn=(os.getenv('SENTRY_DSN') or None))
 
 
 @contextlib.contextmanager
 def args():
-    sys_argv = sys.argv[:]
-    sys.argv = sys.argv[1:]
+    sys.argv, _ = sys.argv[1:], sys.argv[:]
     yield
-    sys.argv = sys_argv
-
-
-def execute(fname):
-    try:
-        execfile(fname)
-    except:
-        print 'catch'
-        raise
+    sys.argv = _
 
 
 def main():
     fname = sys.argv[1]
-    with args():
-        execute(fname)
+    try:
+        with args():
+            execfile(fname)
+    except:
+        client.captureException()
+        raise
 
 if __name__ == '__main__':
     main()
